05e2a6d0de39a8d69eb9f5a619447d5b171fa92c
danielantmann <tula3x3@gmail.com>
2025-10-28 12:08:57 +0100

    Feat/calendar crud (#1)
    * feat(routes): create all events routes
    
    * feat(events): create event model
    
    * feat(event): create service to save events in DB
    
    * feat(events): add update and delete service

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 
+app.use("/api/events", require("./routes/events"));
+
 app.listen(process.env.PORT, () => {
   console.log("servidor en puerto corriendo");
 });
---
+++ routes/events.js
@@ -0,0 +1,45 @@
+//Events Routes
+//host + /api/events
+const { Router } = require("express");
+const { check } = require("express-validator");
+const { jwtValidator } = require("../middlewares/jwt-validator");
+const {
+  createEvent,
+  deleteEvent,
+  getEvents,
+  updateEvent,
+} = require("../controllers/events");
+const { fieldsValidator } = require("../middlewares/fields-validator");
+const { isDate } = require("../helpers/isDate");
+const router = Router();
+
+router.use(jwtValidator);
+
+router.get("/", getEvents);
+
+router.post(
+  "/",
+  [
+    check("title", "Title is required").not().isEmpty(),
+    check("start", "Initial date is required").custom(isDate),
+    check("end", "Final date is required").custom(isDate),
+    fieldsValidator,
+  ],
+
+  createEvent
+);
+
+router.put(
+  "/:id",
+  [
+    check("title", "Title is required").not().isEmpty(),
+    check("start", "Initial date is required").custom(isDate),
+    check("end", "Final date is required").custom(isDate),
+    fieldsValidator,
+  ],
+  updateEvent
+);
+
+router.delete("/:id", deleteEvent);
+
+module.exports = router;
